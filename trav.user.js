3ecc5772e8bd55be5eb44df3abb19195654e0742
Ricardo Brandão <ricardombrandao@gmail.com>
2013-11-04 18:53:22 +0000

    Creation of head script

---
+++ trav.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @author      ScarePT
+// @name		Crop Link Village Travian
+// @namespace	userscripts.org
+// @description	Creates a link to send crop to some village on villages list
+// @include     http://ts2.travianteam.com/*
+// @include     ts2.travianteam.com/*
+// @exclude     http://*.travian.*
+// @exclude     http://www.travian.*
+// @exclude     http://help.travian.*
+// @exclude     http://*.travian.*/logout.php
+// @exclude     http://ads*/*
+// @exclude     http://*survey.travian.*
+// @exclude     http://*travian*anmelden.php
+// @exclude     http://*travian*manual.php*
+// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @grant       none
+// @version     1.0
+// ==/UserScript==
+
+function createLinksToMarket()
+{
+}
+
+createLinksToMarket();
